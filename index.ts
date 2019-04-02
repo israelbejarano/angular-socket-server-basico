37ad263f24271f80d6608a0cfcda81eb2fd8d5ae
israel <israel.bejarano.a@gmail.com>
2019-04-02 23:23:13 +0200

    bodyparser, cors

--- index.ts
+++ index.ts
@@ -1,11 +1,16 @@
-import Server from "./classes/server";
-import router from "./routes/router";
-import bodyParse from 'body-parser';
+import Server from './classes/server';
+import router from './routes/router';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const server = new Server();
 // body parser
-server.app.use(bodyParse.urlencoded({extended: true}));
-server.app.use(bodyParse.json());
+server.app.use(bodyParser.urlencoded({extended: true}));
+server.app.use(bodyParser.json());
+
+// cors
+server.app.use(cors({origin: true, credentials: true}));
+// rutas de servicio
 server.app.use('/', router);
 
 server.start(() => {
