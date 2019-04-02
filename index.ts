2fdd4a6c9fd9b2d62e60811ba791043eefa73155
israel <israel.bejarano.a@gmail.com>
2019-04-02 23:05:12 +0200

    body parser

--- index.ts
+++ index.ts
@@ -1,6 +1,12 @@
 import Server from "./classes/server";
+import router from "./routes/router";
+import bodyParse from 'body-parser';
 
 const server = new Server();
+// body parser
+server.app.use(bodyParse.urlencoded({extended: true}));
+server.app.use(bodyParse.json());
+server.app.use('/', router);
 
 server.start(() => {
     console.log(`Servidor corriendo en puerto ${server.port}`);
