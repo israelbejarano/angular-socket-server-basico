ed1570608647e70e58bef28cfb11bfa4128621cb
israel <israel.bejarano.a@gmail.com>
2019-04-04 22:38:02 +0200

    code refactoring

--- classes/server.ts
+++ classes/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { SERVER_PORT } from '../global/environment';
 import socketIO from 'socket.io';
 import http from 'http';
+import * as socket from '../sockets/socket';
 
 export default class Server {
     private static _instance: Server;
@@ -27,6 +28,9 @@ export default class Server {
         console.log('escuchando conexiones -sockets');
         this.io.on('connection', cliente => {
             console.log('nuevo cliente conectado');
+
+            // desconectar
+            socket.desconectar(cliente);
         });
     }
 
