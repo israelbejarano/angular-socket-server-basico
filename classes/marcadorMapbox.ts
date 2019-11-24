52ea4acaecb60fd2b9de75e15c748c6461993f57
israel <israel.bejarano.a@gmail.com>
2019-11-24 11:08:36 +0100

    mapbox: clase para los marcadores

---
+++ classes/marcadorMapbox.ts
@@ -0,0 +1,10 @@
+export class MarcadorMapbox {
+    constructor(
+        public id: string,
+        public nombre: string,
+        public lat: number,
+        public lng: number,
+        public color: string
+    ) 
+    {}
+}
